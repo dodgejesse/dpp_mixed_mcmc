56b47f2a1bac9c23264d7e61365cb0ced1f45e8f
Jesse Dodge <jessed@cab.ark.cs.cmu.edu>
2017-10-17 14:14:35 -0400

    just some hacky code to run synth experiments

---
+++ synth_experiments/DELETE_ME.py
@@ -0,0 +1,5 @@
+import dpp_rbf_unitcube
+
+tmp = dpp_rbf_unitcube.DPPSampler(20,4)
+import pdb; pdb.set_trace()
+tmp = dpp_rbf_unitcube.DPPSampler(60,4)
---
+++ synth_experiments/dpp_rbf_unitcube.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+cur_dir_path = os.path.dirname(os.path.realpath(__file__))
+
+sys.path.append(cur_dir_path + '/..')
+
+import zero_one_cube_unif_sampler
+import rbf_kernel
+import dpp_mcmc_sampler
+import numpy
+
+
+def DPPSampler(n, d):
+    
+    sampler = zero_one_cube_unif_sampler.Cube_Sampler(d)
+    dist = rbf_kernel.RBF_Kernel()
+
+
+    num_iters = int(max(1000, numpy.power(n,2) * d))
+    
+    B_Y, L_Y, time =  dpp_mcmc_sampler.sample_k_disc_and_cont(sampler, dist, n, num_iters)
+    #print("{} iters took {} seconds".format(num_iters, time))
+    return B_Y
