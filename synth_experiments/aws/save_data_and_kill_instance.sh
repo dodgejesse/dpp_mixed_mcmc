7f8a8e936e408403ae3a13b5154fd2f539ce66fe
EC2 Default User <dodgejesse@gmail.com>
2017-10-20 01:28:59 +0000

    small change to save data

--- synth_experiments/aws/save_data_and_kill_instance.sh
+++ synth_experiments/aws/save_data_and_kill_instance.sh
@@ -4,7 +4,7 @@ CUR_IP=${1}
 SPOT_INST_ID=${2}
 SAVE_LOC=/home/ec2-user/projects/dpp_mixed_mcmc/synth_experiments/pickled_data/origin_center_data
 
-scp -i /home/ec2-user/projects/dpp_mixed_mcmc/synth_experiments/aws/jesse-key-pair-uswest2.pem -oStrictHostKeyChecking=no $SAVE_LOC ec2-user@${CUR_IP}:${SAVE_LOC}
+scp -i /home/ec2-user/projects/dpp_mixed_mcmc/synth_experiments/aws/jesse-key-pair-uswest2.pem -oStrictHostKeyChecking=no $SAVE_LOC/* ec2-user@${CUR_IP}:${SAVE_LOC}
 #ssh -i /home/ec2-user/projects/dpp_mixed_mcmc/synth_experiments/aws/jesse-key-pair-uswest2.pem -oStrictHostKeyChecking=no ec2-user@${CUR_IP} "aws ec2 terminate-instances --instance-ids ${SPOT_INST_ID}"
 
 
