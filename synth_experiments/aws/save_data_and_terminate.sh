49c0a34ea2182f461cb03aabf32f28515e5a4c31
EC2 Default User <dodgejesse@gmail.com>
2017-10-21 02:32:06 +0000

    terminating again

--- synth_experiments/aws/save_data_and_terminate.sh
+++ synth_experiments/aws/save_data_and_terminate.sh
@@ -6,7 +6,7 @@ SAVE_LOC=/home/ec2-user/projects/dpp_mixed_mcmc/synth_experiments/pickled_data/o
 scp -i /home/ec2-user/projects/dpp_mixed_mcmc/synth_experiments/aws/jesse-key-pair-uswest2.pem -oStrictHostKeyChecking=no $SAVE_LOC/* ec2-user@${CUR_IP}:${SAVE_LOC}
 
 
-#aws ec2 terminate-instances --instance-ids $(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+aws ec2 terminate-instances --instance-ids $(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 
 #SPOT_INST_ID=${2}
 #ssh -i /home/ec2-user/projects/dpp_mixed_mcmc/synth_experiments/aws/jesse-key-pair-uswest2.pem -oStrictHostKeyChecking=no ec2-user@${CUR_IP} "aws ec2 terminate-instances --instance-ids ${SPOT_INST_ID}"
